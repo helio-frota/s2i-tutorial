c769fa6dc523a580f6fd49a086213604f57fec63
Helio Frota <00hf11@gmail.com>
2021-08-02 15:10:28 -0300

    test 3

--- base-image/README.md
+++ base-image/README.md
@@ -1,3 +1,9 @@
 # Base image to be used on s2i image
 
 This image is based on fedora.
+
+## Build
+
+docker build -t tutorial/fedora-base .
+
+
--- base-image/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# This script is used to build and test.
-
-echo "-> Building tutorial/fedora-base ..."
-
-docker build -t tutorial/fedora-base .
-
-./test.sh
